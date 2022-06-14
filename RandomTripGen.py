22d7b623f03a3afde741ebfaef010167dc4edfc8
aamrlyman <aamrlyman@gmail.com>
2022-06-13 21:20:10 -0700

    moved display_trip_plan out of master function

--- RandomTripGen.py
+++ RandomTripGen.py
@@ -6,7 +6,8 @@ mode_of_transportations = ['walk', 'bike', 'Uber', 'drive', 'bus']
 entertainments = ['a movie', 'mini golfing', 'a hike', 'a walk through Pike Place Market']
 
 import random
-import weakref
+
+
 def select_mode_of_transportation(list):
     transportation = random.choice(list)
     return transportation
@@ -23,26 +24,24 @@ def select_entertainment(list):
     entertainment = random.choice(list)
     return entertainment
 
-def master_function():
+def display_trip_plan(transport, dining, location, activity): 
+        print('')
+        return print(f'Your itinerary is {transport} to {dining} in {location} followed by {activity}.')
+
+ 
+def generate_trip():
     mode_of_transportation = select_mode_of_transportation(mode_of_transportations)
     restaurant = select_restaurant(restaurants)
     destination = select_destination(destinations)
     entertainment = select_entertainment(entertainments)
-    def trip_plan(): 
-        return f'Your itinerary is {mode_of_transportation} to {restaurant} in {destination} followed by {entertainment}.'
-    print('')
-    print(trip_plan())
-    #User Confirmation 
     is_user_happy = 'no'
-    while is_user_happy == 'no':
-        print('')
+    while is_user_happy != 'yes':
+        display_trip_plan(mode_of_transportation, restaurant, destination, entertainment)
         is_user_happy = input('Are you happy with this itinerary? yes or no: ')
         if is_user_happy == "yes":
-            print('')
-            print(f'{trip_plan()} Have Fun!')
-            print('')
+            display_trip_plan(mode_of_transportation, restaurant, destination, entertainment) 
+            print('Have Fun!')
             break
-        print('')
         user_change = input('What would you like to change? select a number: (1)mode of transportation, (2)restaurant, (3) destination, or (4)entertainment: ')
         if user_change == '1':
             mode_of_transportation = select_mode_of_transportation(mode_of_transportations)
@@ -54,7 +53,6 @@ def master_function():
             entertainment = select_entertainment(entertainments)
         else:
             print('invalid entry')
-            print('')
-        print(trip_plan())
 
-master_function()
+  
+generate_trip()
